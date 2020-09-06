53fe83da2093531a6d898d729131f8b2706ebec0
Mlungisint <69192499+Mlungisint@users.noreply.github.com>
2020-09-06 18:12:06 +0200

    Update IlabWeb2.java

--- IlabWeb2.java
+++ IlabWeb2.java
@@ -12,125 +12,125 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class IlabWeb2 extends utilData {
-	public class IlabWeb2 extends utilData {  
-	static WebDriver driver;
-	static ExtentTest test;
-	static ExtentReports report;
-	// instance variables for data
-	public static String name = "mungisi ntshingila";
-	public static String email = "automationAssessment@iLABQuality.com";
-	public static String phone = "0835687859";
-
-	@BeforeClass
-	public static void startTest()
-	{
-	report = new ExtentReports("C:\\Users\\mlungisi.ntshingila\\Test Reports\\Demo.html");
-	test = report.startTest("ExtentDemo");
+	static WebDriver driver; 
+	
+	
+	
+	public static void main(String[] args) {
+		
+		iLabFirefox run=new iLabFirefox();
+		
+		run.envokeFirefox();//setup method to the open browser
+		run.careerTap();//run career method to click on careers
+		run.captureInf();//run method to populate informtion
+			
+		
 	}
 	
-	
-	@Test
-	public static void setup()
+	public void envokeFirefox()
 	{
-		
-		System.out.println("Start the method to envoke browswer");
-		// Envoke the step 
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\mlungisi.ntshingila\\Documents\\Mlungisi\\selenium\\chromedriver.exe");// set property for
-		driver = new ChromeDriver();
+		//Envoke the step
+		System.setProperty("webdriver.gecko.driver",
+				"C:\\Users\\mlungisi.ntshingila\\Documents\\Mlungisi\\selenium\\geckodriver.exe");		
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		Reporter.log("Chrome Browser is envoked");
-
+		
+		//Open ilab website/url
+		driver.get("https://www.ilabquality.com/.");
+		String title=driver.getTitle();
+		
+		Assert.assertEquals(title, "Home Page | iLAB");//grab the webpage title
+		
 	}
 
-	@Test(dependsOnMethods = "setup")
-	public void testcase1() {
-		String url = utilData.url; // object from ilab website
-		// Open ilab website/url
-		driver.get(url);
-		System.out.println(driver.getTitle());
-		Reporter.log("Ilab website is open");
 		
-		if(driver.getTitle().equals("Home Page | iLAB"))
-		{
-		test.log(LogStatus.PASS, "Navigated iLAB website");
-		}
-		else
-		{
-		test.log(LogStatus.FAIL, "Test Failed");
-		}
-		}
-	
-
-	@Test(dependsOnMethods = "testcase1")
-	public void Testcase2() throws InterruptedException
-
-	{
+	public void careerTap() {
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-		// click on career
+		//click on career
 		driver.findElement(By.linkText("CAREERS")).click();
-		Reporter.log("career link is clicked");
-
-		// Click on the link Souht Africa
+		String careertitle=driver.getTitle();
+		System.out.println(careertitle);
+		
+		
+		//Clikc on the link South Africa
 		driver.findElement(By.linkText("South Africa")).click();
-		Reporter.log("The link South Africa is select/clicked");
-
-		// click on the first job from the list
-		// driver.findElement(By.xpath("//div[@class='wpb_wrapper']/div/div[1]/div[1]")).click();
-		driver.findElement(
-				By.xpath("/html/body/section/div[2]/div/div/div/div[3]/div[2]/div/div/div/div/div/div[1]/div[1]"))
-				.click();
-		Reporter.log("link for the first Job is the list selected/clicked");
-
-		// click the button "apply online"
+		
+		
+		
+		//click on the first job from the list
+		//driver.findElement(By.xpath("//div[@class='wpb_wrapper']/div/div[1]/div[1]")).click();
+		driver.findElement(By.xpath("/html/body/section/div[2]/div/div/div/div[3]/div[2]/div/div/div/div/div/div[1]/div[1]")).click();
+		
+		
+	}
+	
+	public void captureInf() {
+		String pathToLogin = "C:\\Users\\mlungisi.ntshingila\\eclipse-workspace\\VPC_BO\\Data\\Infodata.csv";
+		String name = ReadDataFromCSV("NAME", pathToLogin);
+		String email = ReadDataFromCSV("EMAIL", pathToLogin);
+		String phone = ReadDataFromCSV("EMAIL", pathToLogin);		
+		
+		
+		//click the button "apply online"
 		driver.findElement(By.cssSelector("span.wpjb-glyphs.wpjb-icon-down-open")).click();
-		Reporter.log("Apply Button clicked");
-
-		// pipilate fields
-		driver.findElement(By.id("applicant_name")).sendKeys(ilab2.name);
-
-		driver.findElement(By.id("email")).sendKeys(ilab2.email);
-
-		driver.findElement(By.id("phone")).sendKeys(ilab2.phone);
-
+		Reporter.log("Apply button clicked");
+		
+		
+		//pipilate fields
+		driver.findElement(By.id("applicant_name")).sendKeys(name);
+		
+		driver.findElement(By.id("email")).sendKeys(email);
+		
+		driver.findElement(By.id("phone")).sendKeys(phone);
+		
 		driver.findElement(By.id("wpjb_submit")).click();
-		Reporter.log("All the fields populated with information");
-
-		// capture error message and validate
-		boolean error = driver.findElement(By.xpath("//li[contains(text(),'You need to upload at least one file.')]"))
-				.isDisplayed();// object error element
-		String errorMSG = driver.findElement(By.xpath("//li[contains(text(),'You need to upload at least one file.')]")).getText();
+		Reporter.log("All the fields populated");
 		
-
-		// w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[contains(text(),'You
-		// need to upload at least one file.')]")));
-		System.out.println(errorMSG);
-		System.out.println("Element displayed is :" + error);
-		Reporter.log("error messages displayed to upload docuement");
+		//capture error message and validate
+		boolean error=driver.findElement(By.xpath("//li[contains(text(),'You need to upload at least one file.')]")).isDisplayed();//object error element
+		String actualMsg=driver.findElement(By.xpath("//li[contains(text(),'You need to upload at least one file.')]")).getText();//error content
+		//w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[contains(text(),'You need to upload at least one file.')]")));
+		System.out.println(error);
+		//System.out.println("Element displayed is :"+error);
 		
-		if(errorMSG.equals(("You need to upload at least one file.")))
+		if (actualMsg.contains("You need to upload at least one file."))
 		{
-		test.log(LogStatus.PASS, "Error message displayed");
+			System.out.println("Test Passed");
+			
 		}
 		else
 		{
-		test.log(LogStatus.FAIL, "Test Failed");
+			System.out.println("Test Failed");
 		}
-		}
 		
-		
-		
-		
-
-	// Close the broowser
-	@AfterClass
-	public void CloseBrowser() {
-		report.endTest(test);
-		report.flush();
-		driver.quit();
-		Reporter.log("The broswer is closed");
+		Reporter.log("error message is selected to upload a document");
 	}
+	
+	
+	// Method to read data from CVS
+		public String ReadDataFromCSV(String datatoberead, String datafilepath) {
+
+			String line = "";//
+			String splitBy = ",";//comma seperated value
+			String datatobereturned = "";//actual data/value
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(datafilepath));
+				while ((line = br.readLine()) != null) // returns a Boolean value
+				{
+					String[] employee = line.split(splitBy); // use comma as separator
+
+					if (employee[0].equals(datatoberead)) {
+						// Column that you want to
+						datatobereturned = employee[1];
+					}
+				}
+
+				return datatobereturned;
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+				return "";
+			}
+		}
 
 }
